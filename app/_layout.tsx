c177e7e6a641acc40a638c6384b4c24cc83790ed
JordanGallant <39-12602@sae.edu.za>
2025-02-09 23:12:56 +0200

    login and auth

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,57 @@
 import { Stack } from "expo-router";
+import { ActionSheetProvider } from "@expo/react-native-action-sheet";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
+import * as SecureStore from "expo-secure-store";
+import { SupabaseProvider } from "@/context/SupaBaseContext";
 
-export default function RootLayout() {
-  return <Stack>
-    <Stack.Screen name="index" options={{ headerShown: false }} />
-    <Stack.Screen name="(authenticated)" options={{ headerShown: false }} />
-  </Stack>;
-}
+const CLERK_PUBLISHABLE_KEY = process.env
+  .EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY as string;
+// Cache the Clerk JWT
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+    console.log("tokenCache", tokenCache);
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
+
+const InitalLayout = () => {
+  return (
+    <SupabaseProvider>
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(authenticated)" options={{ headerShown: false }} />
+    </Stack>
+    </SupabaseProvider>
+  );
+};
+
+const RootLayoutNav = () => {
+  return (
+    <ClerkProvider
+      publishableKey={CLERK_PUBLISHABLE_KEY}
+      tokenCache={tokenCache}
+    >
+      <ActionSheetProvider>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <StatusBar style="dark" />
+          <InitalLayout />
+        </GestureHandlerRootView>
+      </ActionSheetProvider>
+    </ClerkProvider>
+  );
+};
+
+export default RootLayoutNav;
--- app/index.tsx
+++ app/index.tsx
@@ -1,53 +1,161 @@
-import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet, Text } from 'react-native';
+import { useCallback, useMemo, useRef, useState } from 'react';
+import { View, StyleSheet, Image, TouchableOpacity, Text } from "react-native";
+import AuthModal from "./components/AuthModal";
+import { Colors } from "../constants/Colors";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { ModalType } from "@/types/enums";
+import * as WebBrowser from "expo-web-browser"
+import { useActionSheet } from "@expo/react-native-action-sheet";
+import {
+  BottomSheetModal,
+  BottomSheetModalProvider,
+  BottomSheetBackdrop,
+} from '@gorhom/bottom-sheet';
+import { BottomSheetProvider } from "@gorhom/bottom-sheet/lib/typescript/contexts";
 
-const LoginScreen = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+const Index = () => {
+  const { top } = useSafeAreaInsets();
+  const {showActionSheetWithOptions} =useActionSheet();
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+  const snapPoints = useMemo(() => ['33%'], []);
+  const [authType, setAuthType] = useState<ModalType | null>(null);
 
-  const handleLogin = () => {
-    console.log('Email:', email);
-    console.log('Password:', password);
+  const showModal = async (type: ModalType) => {
+    setAuthType(type);
+    bottomSheetModalRef.current?.present();
+  };
+  const openLink = () => {
+    WebBrowser.openBrowserAsync('https://google.com')
   };
 
+  const openActionSheet = async () => {
+    const options = ['View support docs', 'Contact us', 'Cancel'];
+    const cancelButtonIndex = 2;
+
+    showActionSheetWithOptions(
+      {
+        options,
+        cancelButtonIndex,
+        title: `Can't log in or sign up?`,
+      },
+      (selectedIndex: any) => {
+        switch (selectedIndex) {
+          case 1:
+            // Support
+            break;
+
+          case cancelButtonIndex:
+          // Canceled
+        }
+      }
+    );
+  };
+  const renderBackdrop = useCallback(
+    (props: any) => (
+      <BottomSheetBackdrop
+        opacity={0.2}
+        appearsOnIndex={0}
+        disappearsOnIndex={-1}
+        {...props}
+        onPress={() => bottomSheetModalRef.current?.close()}
+      />
+    ),
+    []
+  );
   return (
+    <BottomSheetModalProvider>
+
     <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        keyboardType="email-address"
-        autoCapitalize="none"
-        value={email}
-        onChangeText={setEmail}
+      <Image
+        source={require("../assets/images/logo.png")}
+        style={styles.image}
       />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry
-        value={password}
-        onChangeText={setPassword}
-      />
-      <Button title="Login" onPress={handleLogin} />
+
+      <View style={styles.bottomContainer}>
+        <Text style={{ color: "black" }}> log in</Text>
+        <TouchableOpacity
+          style={[styles.btn, { backgroundColor: "black" }]}
+          onPress={() => showModal(ModalType.Login)}
+        >
+          <Text style={styles.text}> log in</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={[styles.btn, { backgroundColor: "white" }]}
+          onPress={() => showModal(ModalType.SignUp)}
+        >
+          <Text style={[styles.text, { color: "black" }]}> Sign Up</Text>
+        </TouchableOpacity>
+        <Text style={[styles.description, { color: "black" }]}>
+          {" "}
+          By signing up you agree to the{" "}
+          <Text style={styles.link} onPress={openLink}>User Notice{" "}</Text>
+          and {''}
+          <Text style={styles.link} onPress={openLink}>Privacy Polocy</Text>
+          .
+        </Text>
+        <Text style={styles.link} onPress={openActionSheet}>cant login or signup?
+        </Text>
+
+      </View>
     </View>
+    <BottomSheetModal
+        ref={bottomSheetModalRef}
+        index={0}
+        snapPoints={snapPoints}
+        handleComponent={null}
+        backdropComponent={renderBackdrop}
+        enableOverDrag={false}
+        enablePanDownToClose>
+        <AuthModal authType={authType} />
+      </BottomSheetModal>
+    </BottomSheetModalProvider>
+
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
+    alignItems: "center",
   },
-  input: {
-    height: 40,
-    width: '100%',
-    borderColor: 'gray',
+  text: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  image: {
+    height: 200,
+    width: 350,
+    resizeMode: "contain",
+    paddingHorizontal: 40,
+  },
+  bottomContainer: {
+    gap: 10,
+    justifyContent: "flex-end",
+    marginBottom: 36,
+    width: "100%",
+    paddingHorizontal: 40,
+  },
+  btn: {
+    padding: 10,
+    borderRadius: 8,
+    alignItems: "center",
+    borderColor: "black",
     borderWidth: 1,
-    marginBottom: 10,
-    paddingHorizontal: 10,
-    borderRadius: 5,
+  },
+  description: {
+    fontSize: 10,
+    textAlign: "center",
+    color: "#fff",
+    marginHorizontal: 60,
+  },
+  link: {
+    color: "purple",
+    fontSize: 10,
+    textAlign: "center",
+    textDecorationLine: "underline",
   },
 });
 
-export default LoginScreen;
+export default Index;
